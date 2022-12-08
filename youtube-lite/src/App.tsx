70db670a6fea805e82155f4602142a8cf98ca65f
Nuno Silva <nuno_nfss96@hotmail.com>
2022-12-08 14:21:45 +0000

    Ran prettier

--- youtube-lite/src/App.tsx
+++ youtube-lite/src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
-const  App = () => {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+        </div>
+    );
+};
 
 export default App;
